dbc1335962d9d69ad100d5c1faf804f70a3d50ca
Kinimotto29 <dommin5@interia.pl>
2017-11-26 10:13:28 +0100

    initt

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    console.log('test');
+    
+    var elementOne = document.getElementById('sectionFirst'); // vanilla JS
+    
+    var elementOneJQuery = $('#sectionFirst'); // JQuery
+    
+    console.log(elementOne);
+    console.log(elementOneJQuery);
+});
